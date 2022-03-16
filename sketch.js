1e1c897bc011e0365facaa88db6d7bdf2a58afd8
Alexander DeRouen <34390214+TreesClouds@users.noreply.github.com>
2022-03-16 12:53:45 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,80 @@
 class Tetromino {
-
+    constructor(type) {
+        this.solid = false;
+        this.active = false;
+        this.outlinesPlaced = false;
+        this.rotation = 0;
+        if (type === undefined) {
+            type = Math.floor(Math.random() * 7);
+        }
+        this.tilePlacement = Tetromino.getCase(type);
+    }
+    
+    rotate(left) {
+        if (left) {
+            this.rotation++;
+            if (this.rotation === this.tilePlacement.length) {
+                this.rotation = 0;
+            }
+        } else {
+            this.rotation--;
+            if (this.rotation === -1) {
+                this.rotation = this.tilePlacement.length-1;
+            }
+        }
+        
+    }
+    
+    static getCase(type) {
+        switch (type) {
+            case 0:
+                return [
+                    [[0, 1, 1], [0, 1, 1], [0, 0, 0]]
+                ];
+            case 1:
+                return [
+                    [[0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0], [0, 0, 0, 0,]],
+                    [[0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0], [0, 0, 1, 0]],
+                    [[0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1], [0, 0, 0, 0]],
+                    [[0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0], [0, 1, 0, 0]]
+                ];
+            case 2:
+                return [
+                    [[1, 1, 0], [0, 1, 1], [0, 0, 0]],
+                    [[0, 0, 1], [0, 1, 1], [0, 1, 0]],
+                    [[0, 0, 0], [1, 1, 0], [0, 1, 1]],
+                    [[0, 1, 0], [1, 1, 0], [1, 0, 0]]
+                ];
+            case 3:
+                return [
+                    [[0, 1, 1], [1, 1, 0], [0, 0, 0]],
+                    [[0, 1, 0], [0, 1, 1], [0, 0, 1]],
+                    [[0, 0, 0], [0, 1, 1], [1, 1, 0]],
+                    [[1, 0, 0], [1, 1, 0], [0, 1, 0]]
+                ];
+            case 4:
+                return [
+                    [[0, 1, 0], [1, 1, 1], [0, 0, 0]],
+                    [[0, 1, 0], [0, 1, 1], [0, 1, 0]],
+                    [[0, 0, 0], [1, 1, 1], [0, 1, 0]],
+                    [[0, 1, 0], [1, 1, 0], [0, 1, 0]]
+                ];
+            case 5:
+                return [
+                    [[0, 0, 1], [1, 1, 1], [0, 0, 0]],
+                    [[0, 1, 0], [0, 1, 0], [0, 1, 1]],
+                    [[0, 0, 0], [1, 1, 1], [1, 0, 0]],
+                    [[1, 1, 0], [0, 1, 0], [0, 1, 0]],
+                ];
+            case 6:
+                return [
+                    [[1, 0, 0], [1, 1, 1], [0, 0, 0]],
+                    [[0, 1, 1], [0, 1, 0], [0, 1, 0]],
+                    [[0, 0, 0], [1, 1, 1], [0, 0, 1]],
+                    [[0, 1, 0], [0, 1, 0], [1, 1, 0]],
+                ];
+        }
+    }
 }
 
 function getPosition(gameX, gameY) {
@@ -15,6 +90,7 @@ let board;
 let tileWidth;
 let xOffset;
 let yOffset;
+let tetrominoes;
 
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
